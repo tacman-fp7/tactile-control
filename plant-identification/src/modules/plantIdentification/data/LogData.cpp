6b64e2012e6c350ba0e2d8698120b0a0e41885a3
Massimo Regoli <massimo.regoli@iit.it>
2015-03-08 19:06:10 +0100

    =minor bug fixes

--- plant-identification/src/modules/plantIdentification/data/LogData.cpp
+++ plant-identification/src/modules/plantIdentification/data/LogData.cpp
@@ -34,28 +34,29 @@ LogData::LogData() {
 
 void LogData::toBottle(Bottle &bottle){
 
+	// #? : parameter number in the log file acquired using datadumper 
 	bottle.clear();
-	bottle.addString(taskId);
-	bottle.addInt(taskType);
-	bottle.addInt(taskOperationMode);
-	bottle.addDouble(targetValue);
+	bottle.addString(taskId); // #3
+	bottle.addInt(taskType); // #4
+	bottle.addInt(taskOperationMode); // #5
+	bottle.addDouble(targetValue); // #6
 	for(int i = 0; i < fingerTaxelValues.size(); i++){
-		bottle.addDouble(fingerTaxelValues[i]);
+		bottle.addDouble(fingerTaxelValues[i]); // #7-18
 	}
-	bottle.addDouble(overallFingerPressure);
-	bottle.addDouble(overallFingerPressureMedian);
-	bottle.addDouble(pwm);
-	bottle.addDouble(realProximalPwm);
-	bottle.addDouble(realDistalPwm);
-	bottle.addDouble(proximalJointAngle);
-	bottle.addDouble(distalJointAngle);
-	bottle.addDouble(pidKpf);
-	bottle.addDouble(pidKif);
-	bottle.addDouble(pidKdf);
-	bottle.addDouble(pidKpb);
-	bottle.addDouble(pidKib);
-	bottle.addDouble(pidKdb);
-	bottle.addDouble(error);
-	bottle.addDouble(errorIntegral);
+	bottle.addDouble(overallFingerPressure); // #19
+	bottle.addDouble(overallFingerPressureMedian); // #20
+	bottle.addDouble(pwm); // #21
+	bottle.addDouble(realProximalPwm); // #22
+	bottle.addDouble(realDistalPwm); // #23
+	bottle.addDouble(proximalJointAngle); // #24
+	bottle.addDouble(distalJointAngle); // #25
+	bottle.addDouble(pidKpf); // #26
+	bottle.addDouble(pidKif); // #27
+	bottle.addDouble(pidKdf); // #28
+	bottle.addDouble(pidKpb); // #29
+	bottle.addDouble(pidKib); // #30
+	bottle.addDouble(pidKdb); // #31
+	bottle.addDouble(error); // #32
+	bottle.addDouble(errorIntegral); // #33
 
 }
