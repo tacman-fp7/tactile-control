627243ef7befa75f27d3fadbd03cef7cf89ddec6
Serena <ivaldi@ias.tu-darmstadt.de>
2015-06-12 20:09:16 +0200

    minor changes

--- gaussian-process-controller/gpControllerRoll1Fing.py
+++ gaussian-process-controller/gpControllerRoll1Fing.py
@@ -3,19 +3,29 @@ import iCubInterface
 import numpy as np
 import yarp
 import time
+import random
 
-def exitModule(state):
-   
-    # TODO
+def exitModule(resetProbability):
+    randomNum = random.random()
+    if randomNum < resetProbability:
+        return True
     return False
+
+def logArray(array,fd):
+    for i in range(len(array)):
+        fd.write(str(array[i]))
+        fd.write(" ")
 
 def main():
 
     # module parameters
     maxIterations = 10000
-    rolloutsNum = 15
-    #                    0   1   2   3   4   5   6   7   8   9  10  11  12  13  14  15 
-    startingPosEncs = [-44, 18, -4, 39,-14,  2,  2, 18, 12, 20,163,  0,  0, 40,  0,  0]   
+    rolloutsNum = 2
+
+    proximalJointStartPos = 40
+    distalJointStartPos = 0
+    #                    0   1   2   3   4   5   6   7   8   9  10  11  12                    13                  14  15
+    startingPosEncs = [-44, 18, -4, 39,-14,  2,  2, 18, 12, 20,163,  0,  0,proximalJointStartPos,distalJointStartPos,  0]   
     
     finger = 1
     proximalJoint = 13
@@ -24,17 +34,23 @@ def main():
     distalJointEnc_1 = 7
     distalJointEnc_2 = 8
 
+    resetProbability = 0.01
+
     actionDuration = 0.1
     pauseDuration = 0.0
 
     stictionVoltage = 100
-    
+    maxVoltage = 300
+
     dataDumperPortName = "/gpc/log:i"
     iCubIconfigFileName = "iCubInterface"
-    gpConfigFileSuffix = "_example"
+    gpConfigFileSuffix = "_init"
   
     jointsToActuate = [proximalJoint,distalJoint]
     
+    outputFileName = "controllerOut/session_" + + "/countroller_output.txt"
+    fd = open("controllerOut/session/countroller_output.txt","w")
+
     # load gaussian process controller
     gp = gpc.GPController(gpConfigFileSuffix)
     gp.load_controller()
@@ -49,11 +65,20 @@ def main():
     # wait for the user
     raw_input("- press enter to start the controller -")
 
+    fd.write("nrollouts: ")
+    fd.write(str(rolloutsNum))
+    fd.write("\n")
+    
     # initialize velocity mode
 #    iCubI.setOpenLoopMode(jointsToActuate)
 
     rolloutsCounter = 0
-    while rolloutsCounter < rolloutsNum
+    while rolloutsCounter < rolloutsNum:
+
+        print "starting iteration n. ",rolloutsCounter + 1
+        fd.write("# HEADER ")
+        fd.write(str(rolloutsCounter + 1))
+        fd.write("\n")
 
         iterCounter = 0
         exit = False
@@ -79,13 +104,21 @@ def main():
             # choose action
             action = gp.get_control(state)
 
-            voltage[0] = voltage[0] + action[0];
-            voltage[1] = voltage[1] + action[1];
-
             # log data
             iCubI.logData(tactileData + encodersData + voltage + [action[0],action[1]])
+            logArray(tactileData,fd)
+            logArray(encodersData,fd)
+            logArray(voltage,fd)
+            logArray(action,fd)
+            fd.write("\n")
 
-            print state,action
+            # update and cut voltage
+            voltage[0] = voltage[0] + action[0];
+            voltage[1] = voltage[1] + action[1];
+            if abs(voltage[0]) > maxVoltage:
+                voltage[0] = maxVoltage*(voltage[0]/abs(voltage[0]))
+            if abs(voltage[1]) > maxVoltage:
+                voltage[1] = maxVoltage*(voltage[1]/abs(voltage[1]))
 
             # apply action
 #            iCubI.openLoopCommand(proximalJoint,voltage[0])        
@@ -96,11 +129,23 @@ def main():
             time.sleep(pauseDuration)
  
             iterCounter = iterCounter + 1
-            exit = exitModule(state)
+            exit = exitModule(resetProbability)
 
-      rolloutsCounter = rolloutsCounter + 1
+        print "finger ripositioning..."
+        # finger repositioning
+#        iCubI.setPositionMode(jointsToActuate)
+#        iCubI.setJointPosition(proximalJoint,0.0)
+#        iCubI.setJointPosition(distalJoint,0.0)
+        time.sleep(5)
+#        iCubI.setJointPosition(proximalJoint,proximalJointStartPos)
+#        iCubI.setJointPosition(distalJoint,distalJointStartPos)
+        time.sleep(5)
+#        iCubI.setOpenLoopMode(jointsToActuate)
+        print "...done"
+        rolloutsCounter = rolloutsCounter + 1
             
 
+    fd.close()
     # restore position mode and close iCubInterface
 #    iCubI.setPositionMode(jointsToActuate)
     iCubI.closeInterface()
--- gaussian-process-controller/testcode.py
+++ gaussian-process-controller/testcode.py
@@ -1,1 +1,4 @@
 print 'ciao'
+read_file = open("controller_example.txt","w")
+print read_file.read()
+read_file.close()
